c68642e6a553c3fcfb0f83dd40b7bbacea637752
MunsMan <munsman.github@gmail.com>
2024-08-18 11:35:45 +0200

    adding Command and Parse Errors

--- src/error.rs
+++ src/error.rs
@@ -25,3 +25,47 @@ impl Display for BatteryError {
 }
 
 impl Error for BatteryError {}
+
+type Command = String;
+type Message = String;
+
+#[derive(Debug)]
+pub enum CommandError {
+    Pidof,
+    AlreadyRunning,
+    HyprlandDispatch(Message),
+    Command(Command, Message),
+}
+
+impl Display for CommandError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let output = match self {
+            CommandError::Pidof => "Unable to run `pidof` command!",
+            CommandError::AlreadyRunning => "Unable to start, because it is already running",
+            CommandError::HyprlandDispatch(message) => &format!("Unable to execute {}", message),
+            CommandError::Command(command, message) => {
+                &format!("Unable to execute {}:\n{}", command, message)
+            }
+        };
+        write!(f, "{}", output)
+    }
+}
+
+impl Error for CommandError {}
+
+#[derive(Debug)]
+pub enum ParseError {
+    Command(String),
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseError::Command(input) => {
+                write!(f, "Unable to Parse Command Input:\nWrong format: {}", input)
+            }
+        }
+    }
+}
+
+impl Error for ParseError {}
