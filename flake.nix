0d58a4d51368d46264153510979d573677917202
MunsMan <munsman.github@gmail.com>
2024-10-30 20:43:00 +0100

    [flake] adding home manager service

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -15,13 +15,37 @@
       in
       with pkgs; {
         devShells.default = mkShell {
-          packages = [ rust rust-analyzer ];
+          packages = [ rust rust-analyzer nixpkgs-fmt ];
         };
         packages.default = rustPlatform.buildRustPackage {
-                        inherit (cargoToml.package) version name;
-                        src = ./.;
-                        cargoLock.lockFile = ./Cargo.lock;
-                        nativeBuildInputs = with pkgs; [ eww pkg-config ];
-                    };
+          inherit (cargoToml.package) version name;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = with pkgs; [ eww pkg-config ];
+        };
+        homeManagerModules.default = { config, lib, pkgs, ... }: {
+          options.services.glue = {
+            enable = lib.mkEnableOption "Glue service";
+          };
+
+          config = lib.mkIf config.services.glue.enable {
+            systemd.user.services.glue = {
+              Unit = {
+                Description = "Glue Daemon Service";
+                After = [ "graphical-session.target" ];
+                PartOf = [ "graphical-session.target" ];
+              };
+              Service = {
+                ExecStart = "${self.packages.${system}.default}/bin/glue deamon";
+                Restart = "always";
+                RestartSec = "10s";
+              };
+              Install = {
+                WantedBy = [ "graphical-session.target" ];
+              };
+            };
+          };
+        };
+        formatter = pkgs.nixfmt-rfc-style;
       });
 }
