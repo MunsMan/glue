9ce13b54c80d2d43746b3e41365713dfca12c64d
MunsMan <munsman.github@gmail.com>
2024-12-11 21:33:52 +0100

    [mic] updating mic settings

--- src/mic.rs
+++ src/mic.rs
@@ -5,36 +5,86 @@ use serde::Serialize;
 use crate::error::{AudioError, CommandError, ParseError};
 use crate::eww::{eww_update, EwwVariable};
 
-#[derive(Serialize, Clone)]
+#[derive(Clone, Debug)]
+pub enum MicState {
+    Muted,
+    Unmuted,
+}
+
+impl MicState {
+    fn toggle(&self) -> Self {
+        match self {
+            MicState::Muted => Self::Unmuted,
+            MicState::Unmuted => Self::Muted,
+        }
+    }
+
+    fn icon(&self) -> char {
+        match self {
+            MicState::Muted => '',
+            MicState::Unmuted => '',
+        }
+    }
+}
+
+impl Serialize for MicState {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_char(self.icon())
+    }
+}
+
+impl From<&MicState> for &'static str {
+    fn from(state: &MicState) -> Self {
+        match state {
+            MicState::Muted => "1",
+            MicState::Unmuted => "0",
+        }
+    }
+}
+
+#[derive(Serialize, Clone, Debug)]
 pub struct MicSettings {
     volume: f32,
-    mute: bool,
-    icon: char,
+    state: MicState,
 }
 
 impl MicSettings {
     fn try_new() -> Result<Self, AudioError> {
         let (volume, mute) = get_mic_info()?;
-        let icon = Self::icon(mute);
-        Ok(Self { volume, mute, icon })
+        Ok(Self {
+            volume,
+            state: mute,
+        })
     }
 
-    fn icon(mute: Mute) -> char {
+    fn icon(mute: &MicState) -> char {
         match mute {
-            true => '',
-            false => '',
+            MicState::Muted => '',
+            MicState::Unmuted => '',
         }
     }
 
     fn update(&self) -> Result<(), CommandError> {
         eww_update(EwwVariable::Mic(self.clone()))
     }
+
+    fn toggle_mute(&mut self) -> Result<(), AudioError> {
+        let _ = Command::new("wpctl")
+            .args(["set-mute", "@DEFAULT_SOURCE@", (&self.state).into()])
+            .spawn()
+            .map_err(|x| CommandError::Command("wpctl set-mute...".to_string(), x.to_string()))
+            .map_err(AudioError::Update);
+        self.state = self.state.toggle();
+        Ok(())
+    }
 }
 
 type MicVolumeLevel = f32;
-type Mute = bool;
 
-fn get_mic_info() -> Result<(MicVolumeLevel, Mute), AudioError> {
+fn get_mic_info() -> Result<(MicVolumeLevel, MicState), AudioError> {
     let output = Command::new("wpctl")
         .args(["get-volume", "@DEFAULT_SOURCE@"])
         .output()
@@ -43,7 +93,11 @@ fn get_mic_info() -> Result<(MicVolumeLevel, Mute), AudioError> {
             |x| String::from_utf8(x.stdout).unwrap(),
         );
     let volume = output.split_whitespace().collect::<Vec<_>>()[1];
-    let mute = output.contains("MUTED");
+    let mute = if output.contains("MUTED") {
+        MicState::Muted
+    } else {
+        MicState::Unmuted
+    };
     let volume = volume
         .parse::<f32>()
         .map_err(|x| ParseError::Volume(volume.to_string(), x.to_string()))
@@ -53,16 +107,16 @@ fn get_mic_info() -> Result<(MicVolumeLevel, Mute), AudioError> {
 
 pub fn get_mic() -> Result<(), AudioError> {
     let settings = MicSettings::try_new()?;
-    settings.update().map_err(AudioError::Command)
+    print!("{}", serde_json::to_string(&settings).unwrap());
+    // settings.update().map_err(AudioError::Command)
+    Ok(())
 }
 
 pub fn toggle_mic() -> Result<(), AudioError> {
-    let _ = Command::new("wpctl")
-        .args(["set-mute", "@DEFAULT_SOURCE@", "toggle"])
-        .spawn()
-        .map_err(|x| CommandError::Command("wpctl set-mute...".to_string(), x.to_string()))
-        .map_err(AudioError::Update)
-        .map(|_| ());
-    let settings = MicSettings::try_new()?;
+    let mut settings = MicSettings::try_new()?;
+    dbg!(&settings);
+    settings.toggle_mute()?;
+    dbg!(&settings);
+    println!("{}", serde_json::to_string(&settings).unwrap());
     settings.update().map_err(AudioError::Command)
 }
