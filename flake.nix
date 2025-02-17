cb5b645f782c1872ddc68c4f630a462241873041
MunsMan <munsman.github@gmail.com>
2025-02-17 19:05:09 +0100

    [coffee] fix typo

--- flake.nix
+++ flake.nix
@@ -107,7 +107,7 @@
           };
 
           config = lib.mkIf cfg.enable {
-            xdg.configFile."glue/config.toml".source = tomlFormat.generate "glue-config" (lib.fiterAttrs (_key: value: value != null) cfg.settings);
+            xdg.configFile."glue/config.toml".source = tomlFormat.generate "glue-config" (lib.filterAttrs (_key: value: value != null) cfg.settings);
             programs.eww = {
               enable = true;
               configDir = ./eww/bar;
