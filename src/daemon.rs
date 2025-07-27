38f439f4369c886b30e7780916e8a8acf1ca673a
MunsMan <munsman.github@gmail.com>
2025-07-27 13:17:15 +0200

    [fix] resolving rust artifacts

--- src/daemon.rs
+++ src/daemon.rs
@@ -37,10 +37,10 @@ pub async fn daemon(
     eww_config: Option<String>,
     no_autostart: bool,
 ) -> Result<(), DaemonError> {
+    let config = Arc::new(config.clone());
     let daemon_id = daemon_id();
     setup_logging(&config, &daemon_id)?;
     eww::open(&eww::WindowName::Bar, eww_config.clone()).map_err(DaemonError::Command)?;
-    let config = Arc::new(config);
     if !no_autostart {
         auto_start(&config).map_err(DaemonError::AutoStart)?;
     }
