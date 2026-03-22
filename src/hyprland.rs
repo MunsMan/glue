03f06126039a43d748caa8e8c3004bb5691a530a
MunsMan <munsman.github@gmail.com>
2026-03-22 17:40:25 +0100

    fix update bugs

--- src/hyprland.rs
+++ src/hyprland.rs
@@ -2,7 +2,7 @@ use hyprland::event_listener::EventListener;
 use log::info;
 use std::{sync::Arc, time::Duration};
 
-use crate::{configuration::Configuration, wake_up, workspace::eww_workspace_update};
+use crate::{configuration::Configuration, workspace::eww_workspace_update};
 
 pub fn listener(config: Arc<Configuration>) -> EventListener {
     let mut listener = EventListener::new();
@@ -11,13 +11,11 @@ pub fn listener(config: Arc<Configuration>) -> EventListener {
         info!("Workspace Switch");
         eww_workspace_update(default_spaces).expect("Unable to update workspace!")
     });
-    let eww_config_monitor_add = config.general.eww_config.clone();
     listener.add_monitor_added_handler(move |data| {
         info!("Monitor {} is added (id: {})", data.name, data.id);
         std::thread::sleep(Duration::from_secs(5));
         // wake_up(eww_config_monitor_add.clone()).expect("Unable to wake up glue!");
     });
-    let eww_config_monitor_remove = config.general.eww_config.clone();
     listener.add_monitor_removed_handler(move |data| {
         info!("Monitor {data} is removed");
         // wake_up(eww_config_monitor_remove.clone()).expect("Unable to wake up glue!");
